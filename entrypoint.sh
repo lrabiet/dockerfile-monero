d088c9a1733c1a6f5501c6847802ffa0220f0e9e
Bryan Stitt <bryan@stitthappens.com>
2017-08-02 09:17:47 -0700

    always setup Tor if env setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,35 +8,35 @@ if [ "${1:0:1}" = '-' ]; then
     set -- monerod "$@"
 fi
 
+if [ -n "$TOR_HOSTNAME" ] || [ -n "$TOR_IP" ]; then
+    # torsocks does not support name resolution so we do it here
+    TORSOCKS_CONF_FILE=/etc/tor/torsocks.conf
+    until [ -n "$TOR_IP" ]; do
+        TOR_IP=$(getent hosts "$TOR_HOSTNAME" | cut -f1 -d' ')
+
+        # TODO: max wait?
+        if [ -z "$TOR_IP" ]; then
+            echo "Tor is not yet running at $TOR_HOSTNAME! Sleeping..."
+            sleep 10
+        fi
+    done
+    echo "TorAddress $TOR_IP" >> "$TORSOCKS_CONF_FILE"
+
+    # torify everything
+    export TORSOCKS_CONF_FILE
+    export LD_PRELOAD=/usr/lib/torsocks/libtorsocks.so
+
+    # make monero work with Tor
+    # https://github.com/monero-project/monero/blob/master/README.md#using-tor
+    export DNS_PUBLIC=tcp
+    export TORSOCKS_ALLOW_INBOUND=1
+fi
+
 # check for the expected command
 if [ "$1" = 'monerod' ]; then
     # TODO: if the config has "rpc-login=monero:changeme", change it automatically
 
     # TODO: optional numactl
-
-    if [ -n "$TOR_HOSTNAME" ]; then
-        # torsocks does not support name resolution so we do it here
-        TORSOCKS_CONF_FILE=/etc/tor/torsocks.conf
-        until [ -n "$TOR_PROXY_IP" ]; do
-            TOR_PROXY_IP=$(getent hosts "$TOR_HOSTNAME" | cut -f1 -d' ')
-
-            # TODO: max wait?
-            if [ -z "$TOR_PROXY_IP" ]; then
-                echo "Tor is not yet running at $TOR_HOSTNAME! Sleeping..."
-                sleep 10
-            fi
-        done
-        echo "TorAddress $TOR_PROXY_IP" >> "$TORSOCKS_CONF_FILE"
-
-        # torify everything
-        export TORSOCKS_CONF_FILE
-        export LD_PRELOAD=/usr/lib/torsocks/libtorsocks.so
-
-        # make monero work with Tor
-        # https://github.com/monero-project/monero/blob/master/README.md#using-tor
-        export DNS_PUBLIC=tcp
-        export TORSOCKS_ALLOW_INBOUND=1
-    fi
 
     # keep config outside the volume
     exec "$@" --config-file $HOME/bitmonero.conf
